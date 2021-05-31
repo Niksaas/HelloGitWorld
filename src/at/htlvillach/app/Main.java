7e1e1bad5aac7580a8704f5c7082c98cf440a11e
Niksaas <pliessnn@edu.htl-villach.at>
2021-05-31 14:36:12 +0200

    edit a new greeting

--- src/at/htlvillach/app/Main.java
+++ src/at/htlvillach/app/Main.java
@@ -3,6 +3,7 @@ package at.htlvillach.app;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Hello Github World");
     }
 }
